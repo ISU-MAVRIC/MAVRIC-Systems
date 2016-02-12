1a615a9185c1aa3a0c1ff50723223e02d01829d8
James Talbert <jtalbert@iastate.edu>
2016-02-12 06:24:25 +0100

    Added GPIO control (using generic nodes)

--- src/mavric/src/listener.cpp
+++ src/mavric/src/listener.cpp
@@ -1,27 +1,31 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Byte.h"
 #include "gpio.h"
 
-#define PIN 2
+int PIN = 2;
 
-void mavric_debug_callback(const std_msgs::String::ConstPtr& msg) {
-  ROS_INFO("Message Recieved: %s", msg->data.c_str());
-  static int state = 0;
-  GPIOWrite(PIN, state);
-  state ^= 1;
+void mavric_debug_callback(const std_msgs::Byte::ConstPtr& msg) {
+  ROS_INFO("Message Recieved: %d", msg->data);
+  GPIOWrite(PIN, msg->data);
 }
 
 int main(int argc, char* argv[]) {
   ros::init(argc, argv, "mstr_cntl_brd_debug");
   ros::NodeHandle thisNode;
 
+  {
+    ros::NodeHandle privateNode("~");  
+    privateNode.getParam("gpio_pin", PIN);
+  }
+  
   GPIOExport(PIN);
   ros::Duration(1).sleep();
   GPIODirection(PIN, OUT);
   GPIOWrite(PIN, 0);
-  
-  ros::Subscriber subscription = thisNode.subscribe("mstr_cntl_brd_debug", 1000, mavric_debug_callback);
-
+  char topicName[100];
+  sprintf(topicName, "mstr_cntl_brd/debug/gpio/%d", PIN);
+  ros::Subscriber subscription = thisNode.subscribe(topicName, 1000, mavric_debug_callback);
+  ROS_INFO("%s", topicName);
   ros::spin();
 
   GPIOUnexport(PIN);
