5c24c57f5f03287756a75e90658dd588c15aece6
James Talbert <jtalbert@iastate.edu>
2017-07-22 05:19:02 +0200

    updated cmake and fixed listeners.cpp compile errors

--- src/mavric/src/listener.cpp
+++ src/mavric/src/listener.cpp
@@ -1,7 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-void mavric_debug_callback(const sdt_msgs::String::ConstPtr& msg) {
+void mavric_debug_callback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("Message Recieved: %s", msg->data.c_str());
 }
 
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   ros::init(argc, argv, "mavric");
   ros::NodeHandle thisNode;
 
-  ros::Subscriber subscription = thisNode.subscribe("mavric_debug", 1000, mavric_debug_callback);
+  ros::Subscriber subscription = thisNode.subscribe("mstr_cntl_brd-debug", 1000, mavric_debug_callback);
 
   ros::spin();
 
