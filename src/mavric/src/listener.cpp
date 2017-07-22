fc43e974b27a5ba1123929fb2aea1cc76041cb6f
James Talbert <jtalbert@iastate.edu>
2017-07-22 04:58:13 +0200

    Fixed error in listener.cpp

--- src/mavric/src/listener.cpp
+++ src/mavric/src/listener.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   ros::init(argc, argv, "mavric");
   ros::NodeHandle thisNode;
 
-  ros::Subscriber subscription = n.subscribe("mavric_debug", 1000, mavric_debug_callback);
+  ros::Subscriber subscription = thisNode.subscribe("mavric_debug", 1000, mavric_debug_callback);
 
   ros::spin();
 
