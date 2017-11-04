c0c42210c32c8a8866732f9ffab8d4f22f3f1ad6
James Talbert <jtalbert@iastate.edu>
2017-11-04 19:33:11 +0100

    Added python nodes.

---
+++ src/mavric/src/translator.py
@@ -0,0 +1,17 @@
+import rospy
+from std_msgs.msg import Strung
+
+def talker():
+	pub = rospy.Publisher('chatter', String, que_size=10)
+	rospy.init_node('talker', anonymous=True)
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		hello_str = "hello world %s" % rospy.get()
+		rospy.loginfo(hello_str)
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException
+		pass
