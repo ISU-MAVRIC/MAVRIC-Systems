90572797241c87a5c0d34620a51b3ac69ab57edb
James Talbert <jtalbert@iastate.edu>
2017-11-08 01:15:25 +0100

    Full pipeline working (Socket->ROS->PWM)

--- src/mavric/src/Publisher.py
+++ src/mavric/src/Publisher.py
@@ -3,18 +3,38 @@
 import rospy
 from std_msgs.msg import String
 
+import socket
+from threading import *
+data = 'stop'
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+host = "127.0.0.1"
+port = 9001
+print (host)
+print (port)
+
+print('where ros starts')
+#<class '__main__.client'>
 def talker():
 	pub = rospy.Publisher("/chatter", String, queue_size=10)
 	rospy.init_node('talker0', anonymous=True)
 	rate = rospy.Rate(10)
+	serversocket.bind((host, port))
+	serversocket.listen(1)
+	rospy.loginfo('server started')
 	while not rospy.is_shutdown():
-		hello_str = "hello world %s" % rospy.get_time()
-		rospy.loginfo(hello_str)
-		pub.publish(hello_str)
+		
+		connection, address = serversocket.accept()
+		data = connection.recv(1024).decode()
+		connection.close()
+		pub.publish(data)
 		rate.sleep()
+	serversocket.close()	
+	
 
 if __name__ == '__main__':
 	try:
 		talker()
 	except rospy.ROSInterruptException:
 		pass
+
--- src/mavric/src/Subscriber.py
+++ src/mavric/src/Subscriber.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
-
+import RPi.GPIO as GPIO
 import rospy
 from std_msgs.msg import String
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+p = GPIO.PWM(12, 50)
+p.start(0)
+
 def callback(data):
 	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+	#PWM
+	number = float(data.data)
+	p.ChangeDutyCycle(number);
+	print(number)
 	
 def listener():
 	rospy.init_node('listener0', anonymous=True)
 	rospy.Subscriber("/chatter", String, callback)
 
 	rospy.spin()
+	GPIO.cleanup()
 
 if __name__ == '__main__':
 	listener()
