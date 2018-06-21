d621cd2b7977431bde0fb900e6fae439b49b0b00
James Talbert <jtalbert@iastate.edu>
2018-06-21 17:17:18 -0500

    Cleaned up a bit

--- src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/catkin_ws/src/mavric
-# Build directory: /home/pi/catkin_ws/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
