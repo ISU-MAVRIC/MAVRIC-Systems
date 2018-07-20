dcff73057813c306584cd8793c1e52c9cba948a4
James Talbert <Talonj123@users.noreply.github.com>
2018-07-20 12:49:01 -0500

    Update Drive_Train_S.py

--- src/mavric/src/Drive_Train_S.py
+++ src/mavric/src/Drive_Train_S.py
@@ -10,11 +10,11 @@ import time
 MinTime = 0.001
 MaxTime = 0.002
 Range   = MaxTime-MinTime
-Period  = 0.004
+Period  = 0.003609
 STOP    = MinTime + Range/2
 
 pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(1/Period)
+pwm.set_pwm_freq(1/0.004)
 
 #convert from % duty cycle to PWM "ticks" for hat
 def to_tick(percent):
@@ -22,24 +22,24 @@ def to_tick(percent):
         time = Range*percent + MinTime
         percent = time/Period
         # the 0.92 is a fudge factor that is needed for unokown reasons
-        return int(4095 * percent / 0.92)
+        return int(4095 * percent)
 
 # Takes in throttles as percents in the range [-1.0, +1.0]
 #     LF, LM, LB, RF, RM, RB
 def set_outputs(LF, LM, LB, RF, RM, RB):
-        LF_Chan = rospy.get_param("/Drive/Left_Front/Channel", -1)
-        LM_Chan = rospy.get_param("/Drive/Left_Middle/Channel", -1)
-        LB_Chan = rospy.get_param("/Drive/Left_Back/Channel", -1)
-        RF_Chan = rospy.get_param("/Drive/Right_Front/Channel", -1)
-        RM_Chan = rospy.get_param("/Drive/Right_Middle/Channel", -1)
-        RB_Chan = rospy.get_param("/Drive/Right_Back/Channel", -1)
-        Scale = rospy.get_param("/Drive/Range", 0.4)
-        LF_Dir = rospy.get_param("/Drive/Left_Front/Dir", 1)
-        LM_Dir = rospy.get_param("/Drive/Left_Middle/Dir", 1)
-        LB_Dir = rospy.get_param("/Drive/Left_Back/Dir", 1)
-        RF_Dir = rospy.get_param("/Drive/Right_Front/Dir", 1) * -1
-        RM_Dir = rospy.get_param("/Drive/Right_Middle/Dir", 1) * -1
-        RB_Dir = rospy.get_param("/Drive/Right_Back/Dir", 1) * -1
+        global LF_Chan
+        global LM_Chan
+        global LB_Chan
+        global RF_Chan
+        global RM_Chan
+        global RB_Chan
+        global Scale
+        global LF_Dir
+        global LM_Dir
+        global LB_Dir
+        global RF_Dir
+        global RM_Dir
+        global RB_Dir
 
 
         if (LF_Chan >= 0):
@@ -55,7 +55,6 @@ def set_outputs(LF, LM, LB, RF, RM, RB):
         if (RB_Chan >= 0):
                 pwm.set_pwm(RB_Chan, 0, to_tick(RB*Scale*RB_Dir/2+0.5))
         return
-
 
 def callback(data):
         print("Left: " + str(data.left) + ", Right: " + str(data.right))
@@ -79,15 +78,41 @@ def callback(data):
         rospy.loginfo(rospy.get_caller_id() + " Left %s%%, Right %s%%", left*100, right*100)
         set_outputs(left, left, left, right, right, right)
 
-
-
 def listener():
+        global LF_Chan
+        global LM_Chan
+        global LB_Chan
+        global RF_Chan
+        global RM_Chan
+        global RB_Chan
+        global Scale
+        global LF_Dir
+        global LM_Dir
+        global LB_Dir
+        global RF_Dir
+        global RM_Dir
+        global RB_Dir
+
         rospy.init_node('DTS')
-        rospy.Subscriber("/Drive_Train", Drivetrain, callback)
-        time.sleep(10)
+        rospy.Subscriber("/Drive_Train", Drivetrain, callback, queue_size=10)
+        #time.sleep(10)
+        LF_Chan = rospy.get_param("/Drive/Left_Front/Channel", -1)
+        LM_Chan = rospy.get_param("/Drive/Left_Middle/Channel", -1)
+        LB_Chan = rospy.get_param("/Drive/Left_Back/Channel", -1)
+        RF_Chan = rospy.get_param("/Drive/Right_Front/Channel", -1)
+        RM_Chan = rospy.get_param("/Drive/Right_Middle/Channel", -1)
+        RB_Chan = rospy.get_param("/Drive/Right_Back/Channel", -1)
+        Scale = rospy.get_param("/Drive/Range", 0.4)
+        LF_Dir = rospy.get_param("/Drive/Left_Front/Dir", 1)
+        LM_Dir = rospy.get_param("/Drive/Left_Middle/Dir", 1)
+        LB_Dir = rospy.get_param("/Drive/Left_Back/Dir", 1)
+        RF_Dir = rospy.get_param("/Drive/Right_Front/Dir", 1) * -1
+        RM_Dir = rospy.get_param("/Drive/Right_Middle/Dir", 1) * -1
+        RB_Dir = rospy.get_param("/Drive/Right_Back/Dir", 1) * -1
         set_outputs(0, 0, 0, 0, 0, 0)
 
         rospy.spin()
+
 
 
 if __name__ == '__main__':
