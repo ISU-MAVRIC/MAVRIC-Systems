cf0ec0acbcc3f4411d8313613e4206802cc2968c
James Talbert <jtalbert@iastate.edu>
2018-07-28 23:04:07 +0200

    Added a temperature endpoint

--- src/mavric/src/Temp_Interface.py
+++ src/mavric/src/Temp_Interface.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 import rospy
-from mavric.msg import GPS
+from std_msgs.msg import Float64
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 host = ""
-port = 8001
+port = 8002
 print (host)
 print (port)
 
 clients = [];
 
 def callback(data):
-        message = '%.10f,%.10f,%.3f,%.2f,%1f,%d\r\n' % (data.latitude, data.longititude, data.altitude, data.speed, data.heading, data.num_satellites)
+        message = '%.3f\r\n' % (data.data)
         for client in clients:
                 try:
                         client.sendall(message.encode())
@@ -27,15 +27,15 @@ def callback(data):
                         print('%d clients remain\n' % len(clients))
 
 def talker():
-	rospy.init_node('GPS_Interface')
-        rospy.Subscriber("GPS_Data", GPS, callback, queue_size=10)
+	rospy.init_node('Temperature_Interface')
+        rospy.Subscriber("Temperature", Float64, callback, queue_size=10)
 	serversocket.bind((host, port))
 	serversocket.listen(1)
 	rospy.loginfo('server started')
 	while not rospy.is_shutdown():
 		connection, address = serversocket.accept()
 		clients.append(connection)
-                print('new GPS Listener: ')
+                print('new Temperature Listener: ')
                 print(address)
                 print('\n')
                 
