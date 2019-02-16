dd4e871a634077ce8e962cf93276518aebbe7a9d
James Talbert <jtalbert@iastate.edu>
2019-02-16 23:43:54 +0100

    Update LTC2497 ADC driver, range appears to be 0->2.5V

--- src/mavric/src/LTC2497_ADC_HAT.py
+++ src/mavric/src/LTC2497_ADC_HAT.py
@@ -3,14 +3,12 @@
 import rospy
 import time
 from std_msgs.msg import Float64
-from smbus import SMBus
+import pigpio
 
-bus = SMBus(1)
+pi = pigpio.pi()
+bus = pi.i2c_open(1, 0x76)
 
 adc_address = 0x76
-
-channels = [0xB0, 0xB8, 0xB1, 0xB9, 0xB2, 0xBA, 0xB3, 0xBB,
-            0xB4, 0xBC, 0xB5, 0xBD, 0xB6, 0xBE, 0xB7, 0xBF]
 
 channel_multipliers = [1, 1, 1, 1, 1, 1, 1, 1,
                        1, 1, 1, 1, 1, 1, 1, 1]
@@ -20,18 +18,20 @@ publishers = []
 vref = 5
 max_reading = 8388608.0
 
-i2c_read_delay = 0.0588
+i2c_read_delay = 0.15
 i2c_read_bytes = 0x06
 
 def read_adc(adc_address, adc_channel):
-    bus.write_byte(adc_address, adc_channel)
+    time.sleep(i2c_read_delay)
+    #print(adc_channel)
+    pi.i2c_write_byte(bus, (0b10110000) | (adc_channel & 0x0F))
     
     time.sleep(i2c_read_delay)
 
-    reading = bus.read_i2c_block_data(adc_address, adc_channel, i2c_read_bytes)
-
+    (count, reading) = pi.i2c_read_device(bus, i2c_read_bytes)
     valor = ( ((reading[0]&0x3F) << 16) + (reading[1] << 8) + (reading[2]&0xE0) )
     volts = valor * vref / max_reading
+    print(volts)
 
     if((reading[0] & 0b11000000) == 0b11000000):
         #handle read error
@@ -47,13 +47,13 @@ def talker():
 
     rospy.init_node('LTC2497_ADC_HAT')
 
-    frequency = rospy.get_param("~frequency", 10);
+    frequency = rospy.get_param("~frequency", 1/i2c_read_delay);
 
     rate = rospy.Rate(frequency)
 
     while not rospy.is_shutdown():
         for i in range(0, 2):  #supports 16, 2 for testing
-            adc_val = multipliers[i] * read_adc(adc_address, channels[i])
+            adc_val = channel_multipliers[i] * read_adc(adc_address, i)
             publishers[i].publish(adc_val)
 
         rate.sleep()
