414ae5721b1edaca8f7443d45ab4c9f00b00ccbd
MAVRIC Jetson TX2 <jtalbert123@gmail.com>
2020-02-22 12:38:08 -0600

    Added Robot Enable node to send Talon SRX heartbeat over CAN

---
+++ src/mavric/src/CAN_RobotEnable.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# CAN Robot Enable controller. Implements the robot enable heartbeat required by the Talon SRXs when controlled over CAN. If the Talons do not receive this heartbeat every 100ms, they will shut down.
+
+# Topics:
+#   Enabled - Subscription: Listens for input from the user to enable or disable the rover. Defaults to enabled, relying on the base station to 
+
+import rospy
+import os
+from std_msgs.msg import Bool
+import time
+
+enabled = False
+
+def callback(data):
+        global enabled
+	enabled = data.data
+
+def listener():
+        global enabled
+
+        rospy.init_node('CAN_REn')
+        rospy.Subscriber("CAN/TalonsEnabled", Bool, callback, queue_size=10)
+
+        r = rospy.Rate(100) #must send command within 100ms of the last one
+        
+        while not rospy.is_shutdown():
+		if(enabled):
+			#command read off the CAN bus using canqv
+			#command originates from the Phoenix Tuner app on Windows
+			os.system("cansend can0 000401bf#0100");
+                        
+                r.sleep()
+                
+if __name__ == '__main__':
+    listener()
