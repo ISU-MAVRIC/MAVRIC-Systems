0eef2833a373e0bbb0fee5ce97d9df4ffd47c104
Gabe C <gcar2101@gmail.com>
2021-10-12 14:38:42 -0500

    Fixed initial commit

--- src/mavric/src/Relay_Switcher.py
+++ src/mavric/src/Relay_Switcher.py
@@ -1,17 +1,21 @@
 import rospy
+import Jetson.GPIO as GPIO
 from std_msgs.msg import Bool
 
 config = {
     "Lightbar": 1
 }
 
-def callback(data):
-    print(data)
+def callback(data, args):
+    print(args[1], data.data)
+    #GPIO.output(args[1], data.data)
 
 def talker():
-        rospy.init_node('Float64_Heartbeat')
+        rospy.init_node('Relay_Switcher')
+        GPIO.setmode(GPIO.BCM)
         for relay_name, relay_pin in config.items():
             rospy.Subscriber('/Relay/' + relay_name, Bool, callback, (relay_name, relay_pin))
+            GPIO.setup(relay_pin, GPIO.OUT)
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
                 r.sleep()
