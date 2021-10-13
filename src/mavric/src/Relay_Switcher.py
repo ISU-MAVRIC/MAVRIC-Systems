030391ba513964470f36cbd24d24dc312b0200c0
mavric <peskuski@iastate.edu>
2021-10-13 16:17:31 -0500

    ubleshooted relay switcher

--- src/mavric/src/Relay_Switcher.py
+++ src/mavric/src/Relay_Switcher.py
@@ -4,22 +4,23 @@ import Jetson.GPIO as GPIO
 from std_msgs.msg import Bool
 
 config = {
-    "Lightbar": 1
+    "Lightbar": 16
 }
 
 def callback(data, args):
     print(args[1], data.data)
-    #GPIO.output(args[1], data.data)
+    GPIO.output(args[1], GPIO.HIGH if data.data else GPIO.LOW)
 
 def talker():
         rospy.init_node('Relay_Switcher')
-        GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BOARD)
         for relay_name, relay_pin in config.items():
-            rospy.Subscriber('/Relay/' + relay_name, Bool, callback, (relay_name, relay_pin))
             GPIO.setup(relay_pin, GPIO.OUT)
+            rospy.Subscriber('/Relay/' + relay_name, Bool, callback, (relay_name, relay_pin))
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
                 r.sleep()
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     talker()
