f2e9082133467c0d4665b9fff7c4d2bea2890d22
Jacob Peskuski <peskuski@iastate.edu>
2021-11-02 15:21:22 -0500

    Modified IMU code to work with I2c

--- src/mavric/src/BNO055_IMU.py
+++ src/mavric/src/BNO055_IMU.py
@@ -4,18 +4,22 @@ import rospy
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Vector3
 from Adafruit_BNO055 import BNO055
+import board
 
-bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+i2c = board.I2C()
+bno = BNO055.BNO055_I2C(i2c)
 
 pwm_offset_ms = 0
 
+
 def angle_to_ms(angle):
-    #convert from (-90 to 90) to about (1ms to 2ms)
+    # convert from (-90 to 90) to about (1ms to 2ms)
     percent = (angle / 180.0) + 0.5
     return ((1 + percent) * 0.001) + (pwm_offset_ms * 0.001)
 
+
 def servo_cal_roll(pub):
-    #allow IMU to auto-calibrate roll angle
+    # allow IMU to auto-calibrate roll angle
     pub.publish(angle_to_ms(-60))
     rospy.sleep(0.5)
 
@@ -25,38 +29,36 @@ def servo_cal_roll(pub):
     pub.publish(angle_to_ms(0))
     rospy.sleep(0.5)
 
+
 def talker():
     global pwm_offset_ms
 
-    #define custom 3-variable message type for this
+    # define custom 3-variable message type for this
     pub_sys_cal = rospy.Publisher("IMU/SysCalibration", Float64, queue_size=10)
-    pub_cal     = rospy.Publisher("IMU/SensorCalibrations", Vector3, queue_size=10)
+    pub_cal = rospy.Publisher("IMU/SensorCalibrations", Vector3, queue_size=10)
 
     pub_angle = rospy.Publisher("IMU/FusedAngle", Vector3, queue_size=10)
 
-    pub_gyro_raw  = rospy.Publisher("IMU/RawGyro", Vector3, queue_size=10)
-    pub_accel_raw = rospy.Publisher("IMU/RawAccel", Vector3, queue_size=10)
-    pub_mag_raw   = rospy.Publisher("IMU/RawMag", Vector3, queue_size=10)
+    #pub_gyro_raw = rospy.Publisher("IMU/RawGyro", Vector3, queue_size=10)
+    #pub_accel_raw = rospy.Publisher("IMU/RawAccel", Vector3, queue_size=10)
+    #pub_mag_raw = rospy.Publisher("IMU/RawMag", Vector3, queue_size=10)
 
     pub_servo = rospy.Publisher("IMU_Cal_Servo", Float64, queue_size=10)
 
     rospy.init_node('BNO055_IMU')
 
-    pwm_offset_ms = rospy.get_param("~pwm_offset_ms", 0) #0.10
+    pwm_offset_ms = rospy.get_param("~pwm_offset_ms", 0)  # 0.10
 
     rate = rospy.Rate(20)
 
-    if not bno.begin():
-        raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
-
-    #wiggle servo
+    # wiggle servo
     servo_cal_roll(pub_servo)
 
     while not rospy.is_shutdown():
-        sys_cal, gyro_cal, accel_cal, mag_cal = bno.get_calibration_status()
-        yaw, roll, pitch = bno.read_euler()
+        sys_cal, gyro_cal, accel_cal, mag_cal = bno.calibration_status()
+        yaw, roll, pitch = bno.euler()
 
-        #if necessary
+        # if necessary
         #m_x, m_y, m_z = bno.read_magnetometer()
         #g_x, g_y, g_z = bno.read_gyroscope()
         #a_x, a_y, a_z = bno.read_accelerometer()
@@ -67,7 +69,7 @@ def talker():
         pub_cal.publish(gyro_cal, accel_cal, mag_cal)
         pub_angle.publish(roll, pitch, yaw)
 
-        #if necessary
+        # if necessary
         #pub_gyro_raw.publish(g_x, g_y, g_z)
         #pub_accel_raw.publish(a_x, a_y, a_z)
         #pub_mag_raw.publish(m_x, m_y, m_z)
