a4de8622b0d7b56dc8fce59453f6985c4f08877b
Gabe C <gcar2101@gmail.com>
2022-02-15 19:57:54 -0600

    Basic arUco detection

---
+++ src/mavric/src/Autonomous/ArucoDetectionThread.py
@@ -0,0 +1,60 @@
+from imutils.video import VideoStream
+import imutils
+import time
+import cv2
+import threading
+import auto_globals
+
+
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+def get_markers_from_frame(frame):
+    markerLocations = []
+    markerIds = []
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
+    if (len(corners) > 0):
+        ids = ids.flatten()
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            # compute and draw the center (x, y)-coordinates of the
+            # ArUco marker
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            markerLocations.append(cX, cY)
+            markerIds.append(markerID)
+    return (markerIds, markerLocations)
+
+
+
+def aruco_detection():
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+    arucoParams = cv2.aruco.DetectorParameters_create()
+    vs = VideoStream(src=0).start()
+    vs2 = VideoStream(src=1).start()
+    time.sleep(1)
+    while True:
+        frame = vs.read()
+        frame2 = vs.read()
+        markers1 = get_markers_from_frame(frame)
+        markers2 = get_markers_from_frame(frame)
+        markers = []
+        for index, ID in enumerate(markers2[0]):
+            if ID not in markers1[0]:
+                markers1[0].append(markers2[0][index])
+                markers1[1].append(markrs2[1][index])
+        auto_globals.markers = markers
+
+    vs.stop()
+    vs2.stop()
+
+thread = threading.Thread(target=aruco_detection)
+
+def start_aruco_detection():
+    thread.start()
+
+start_aruco_detection()
