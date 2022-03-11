94671c1e128cf6c72a2b7a85380d33fde9bd0f0c
JEP830 <trumpeter830@gmail.com>
2022-03-10 18:12:06 -0600

    Troubleshooted PID test. Found some decent K constants and found some issues with encoder feedback.

--- src/mavric/src/LinearMapping.py
+++ src/mavric/src/LinearMapping.py
@@ -4,41 +4,46 @@ import rospy
 from std_msgs.msg import Float64
 from std_msgs.msg import Bool
 
-def callback(data, (pub, slope, intercept)):
-        pub.publish(data.data*slope+intercept)
+
+def callback(data, args):
+    pub = args[0]
+    dat = data.data
+    pub.publish(dat*args[1]+args[2])
+
 
 def talker():
-        global low_limit
-        global slope
-        global intercept
-        
-        rospy.init_node('LinearMapping')
-        inputs = rospy.get_param('~inputs', '').split(',')
-        outputs = rospy.get_param('~outputs', '').split(',')
+    global low_limit
+    global slope
+    global intercept
 
-        slopes = rospy.get_param('~slopes', '0')
-        if type(slopes) == str:
-                slopes = map(float,slopes.split(','))
-        else:
-                slopes = [slopes]
-                
-        intercepts = rospy.get_param('~intercepts', '0')
-        if type(intercepts) == str:
-                intercepts = map(float,intercepts.split(','))
-        else:
-                intercepts = [intercepts]
+    rospy.init_node('LinearMapping')
+    inputs = rospy.get_param('~inputs', 'q')
+    outputs = rospy.get_param('~outputs', 'q')
 
-        if len(inputs) != len(outputs):
-                raise ValueError("The inputs and outputs are not the same size")
-        if len(inputs) != len(slopes):
-                raise ValueError("The inputs and slopes are not the same size")
-        if len(inputs) != len(intercepts):
-                raise ValueError("The inputs and intercepts are not the same size")
-                                 
-        for i in range(len(inputs)):
-                output_topic = rospy.Subscriber(inputs[i], Float64, callback, (rospy.Publisher(outputs[i], Float64, queue_size=10), slopes[i], intercepts[i]), queue_size=10)
+    slopes = rospy.get_param('~slopes', '0')
+    if type(slopes) == str:
+        slopes = float(slopes)
+    else:
+        slopes = slopes
 
-        rospy.spin()
-        
+    intercepts = rospy.get_param('~intercepts', '0')
+    if type(intercepts) == str:
+        intercepts = float(intercepts)
+    else:
+        intercepts = intercepts
+
+    #if len(inputs) != len(outputs):
+        #raise ValueError("The inputs and outputs are not the same size")
+    #if len(inputs) != len(slopes):
+        #raise ValueError("The inputs and slopes are not the same size")
+    #if len(inputs) != len(intercepts):
+        #raise ValueError("The inputs and intercepts are not the same size")
+
+    output_topic = rospy.Subscriber(inputs, Float64, callback, (rospy.Publisher(
+            outputs, Float64, queue_size=10), slopes, intercepts), queue_size=10)
+
+    rospy.spin()
+
+
 if __name__ == '__main__':
     talker()
