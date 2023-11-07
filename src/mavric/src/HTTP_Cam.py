481366c163c55f9bee32173b1c09c64e83d49fac
Nathan Logston <natestorms416@gmail.com>
2023-11-06 20:40:37 -0600

    Finalized the camera script

--- src/mavric/src/HTTP_Cam.py
+++ src/mavric/src/HTTP_Cam.py
@@ -1,49 +1,40 @@
 #!/usr/bin/env python3
 
 import subprocess
-import time
-import cv2
 import rospy
+import time
 
-
-def detect_camera_location():
-    camera_location = 0 # Default camera location is 0
-    for i in range(1):  # Checks for camera on video0 or video1
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            camera_location = i
-            cap.release()
-            break
-    return camera_location
-
+# Checks for keyboard inturpt every second and then will kill MJPG Streamer process
 def shutdown_mjpg_streamer():
     try:
         subprocess.run(["pkill", "-f", "mjpg_streamer"])
         print("MJPG-Streamer has been shut down.")
     except Exception as e:
         print("Error shutting down MJPG-Streamer")
+        
+if __name__ == '__main__':
+    try:
+        # Camera input settings
+        rospy.init_node('arm_camera')
+        resolution = rospy.get_param('~resolution', '1280x720')
+        frame_rate = rospy.get_param('~frame_rate', '30')
+        port = rospy.get_param('~port', '8080')
+        camera_location = rospy.get_param('~video_device', '0')
 
+        # Creates HTTP Stream from location of the camera using MJPG Streamer
+        mjpg_streamer_command = f"mjpg_streamer -i 'input_uvc.so -d /dev/video{camera_location} -r {resolution} -f {frame_rate}' -o 'output_http.so -w /home/mavric/mjpg-streamer/mjpg-streamer-experimental/www -p {port}'"
 
-camera_location = detect_camera_location()
+        # Starts MJPG Streamer as a subprocess
+        try:
+            subprocess.Popen(mjpg_streamer_command, shell=True)
+            
+            print("MJPG Streamer is running")
+        except:
+            print("There was an error trying to run MJPG Streamer.")
 
-# Camera input settings
-resolution = rospy.get_param('~armCameraResolution')
-frame_rate = rospy.get_param('~armCameraFrameRate')
+        while True:
+            time.sleep(1)
 
-# Creates HTTP Stream from location of the camera using MJPG Streamer
-mjpg_streamer_command = f"mjpg_streamer -i 'input_uvc.so -d /dev/video{camera_location} -r {resolution} -f {frame_rate}' -o 'output_http.so -w /home/mavric/mjpg-streamer/mjpg-streamer-experimental/www'"
-
-# Starts MJPG Streamer as a subprocess
-try:
-    subprocess.Popen(mjpg_streamer_command, shell=True)
-    print("MJPG Streamer is running")
-except:
-    print("There was an error trying to run MJPG Streamer.")
-
-
-# Checks for keyboard inturpt every second and then will kill MJPG Streamer process
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    shutdown_mjpg_streamer()
+    except rospy.ROSInterruptException:
+        shutdown_mjpg_streamer()
+        pass
