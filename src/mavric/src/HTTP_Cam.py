5de733a40f928b058ccac0adb7a2ab8f6d5f44f8
jmilburn7 <143975443+jmilburn7@users.noreply.github.com>
2023-11-13 22:59:32 -0600

    Update HTTP_Cam.py
    Added additional comments

--- src/mavric/src/HTTP_Cam.py
+++ src/mavric/src/HTTP_Cam.py
@@ -4,7 +4,7 @@ import subprocess
 import rospy
 import time
 
-# Checks for keyboard inturpt every second and then will kill MJPG Streamer process
+# Shuts down the MJPG Subprocess
 def shutdown_mjpg_streamer():
     try:
         subprocess.run(["pkill", "-f", "mjpg_streamer"])
@@ -14,27 +14,28 @@ def shutdown_mjpg_streamer():
         
 if __name__ == '__main__':
     try:
-        # Camera input settings
+        # Camera input settings from ROS Camera node
         rospy.init_node('arm_camera')
         resolution = rospy.get_param('~resolution', '1280x720')
         frame_rate = rospy.get_param('~frame_rate', '30')
         port = rospy.get_param('~port', '8080')
         camera_location = rospy.get_param('~video_device', '0')
 
-        # Creates HTTP Stream from location of the camera using MJPG Streamer
+        # Command for creating HTTP Stream of a camera using MJPG Streamer based on ROS parameters
         mjpg_streamer_command = f"mjpg_streamer -i 'input_uvc.so -d /dev/video{camera_location} -r {resolution} -f {frame_rate}' -o 'output_http.so -w /home/mavric/mjpg-streamer/mjpg-streamer-experimental/www -p {port}'"
 
-        # Starts MJPG Streamer as a subprocess
+        # Runs the MJPG Stream command as a subprocess
         try:
             subprocess.Popen(mjpg_streamer_command, shell=True)
-            
             print("MJPG Streamer is running")
         except:
             print("There was an error trying to run MJPG Streamer.")
 
+        # Keeps MJPG running if there's no exception
         while True:
             time.sleep(1)
 
+    # Calls the MJPG Shutdown function if there is an interruption from ROS
     except rospy.ROSInterruptException:
         shutdown_mjpg_streamer()
         pass
