3ec046e994d88a81d233ebb330f8fe1dab51018b
jmilburn7 <143975443+jmilburn7@users.noreply.github.com>
2023-11-27 23:22:02 -0600

    Update HTTP_Cam.py
    Added more detailed comments to finalize the script

--- src/mavric/src/HTTP_Cam.py
+++ src/mavric/src/HTTP_Cam.py
@@ -15,23 +15,24 @@ def shutdown_mjpg_streamer():
 if __name__ == '__main__':
     try:
         # Camera input settings from ROS Camera node
-        rospy.init_node('arm_camera')
-        resolution = rospy.get_param('~resolution', '1280x720')
-        frame_rate = rospy.get_param('~frame_rate', '30')
-        port = rospy.get_param('~port', '8080')
-        camera_location = rospy.get_param('~video_device', '0')
+        rospy.init_node('arm_camera') # Initializes arm_camera node
+        resolution = rospy.get_param('~resolution', '1280x720') # Pulls resolution from ros and includes a default value
+        frame_rate = rospy.get_param('~frame_rate', '30') # Pulls frame rate from ros and includes a default value
+        port = rospy.get_param('~port', '8080') # Pulls port from ros and includes a default value
+        camera_location = rospy.get_param('~video_device', '0') # Pulls camera device location from ros and includes a default value
 
         # Command for creating HTTP Stream of a camera using MJPG Streamer based on ROS parameters
         mjpg_streamer_command = f"mjpg_streamer -i 'input_uvc.so -d /dev/video{camera_location} -r {resolution} -f {frame_rate}' -o 'output_http.so -w /home/mavric/mjpg-streamer/mjpg-streamer-experimental/www -p {port}'"
 
         # Runs the MJPG Stream command as a subprocess
         try:
+            # Utilizes the subprocess module to run the mjpg_streamer command
             subprocess.Popen(mjpg_streamer_command, shell=True)
             print("MJPG Streamer is running")
         except:
             print("There was an error trying to run MJPG Streamer.")
 
-        # Keeps MJPG running if there's no exception
+        # Keeps MJPG running unless there is an exception
         while True:
             time.sleep(1)
 
