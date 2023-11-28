9f809a5625ece7f6f2ef5ebbc7600a786033b3a2
Nathan Logston <natestorms416@gmail.com>
2023-11-27 21:38:47 -0600

    changed debug statements

--- src/mavric/src/900MHz_Interface.py
+++ src/mavric/src/900MHz_Interface.py
@@ -45,9 +45,8 @@ def client_cb(data):
 if __name__=='__main__':
     try:
         # Publishers and Subscribers
-        drive = rospy.Publisher("Drive_Train", Drivetrain, queue_size=10)
-        steer = rospy.Publisher("Steer_Train", Steertrain, queue_size=10)
-        status = rospy.Publisher("LoRa_Status", String, queue_size=10)
+        drive = rospy.Publisher("/Drive/Drive_Command", Drivetrain, queue_size=10)
+        steer = rospy.Publisher("/Drive/Steer_Command", Steertrain, queue_size=10)
         clients = rospy.Subscriber("client_count", Int32, client_cb, queue_size=10)
         rospy.init_node("LoRa_Radio")
 
@@ -56,9 +55,9 @@ if __name__=='__main__':
         link.open() # open link between rover and LoRa
 
         while not rospy.is_shutdown():
-            status.publish("Starting Loop")
+            print("Starting Loop")
             if link.available():    # Recieve new data from serial
-                status.publish("Data Recieved")
+                print("Data Recieved")
                 recSize = 0
 
                 dataRX.drive = link.rx_obj(obj_type='f', start_pos=recSize)
@@ -71,7 +70,7 @@ if __name__=='__main__':
                 recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
 
                 #print('drive: {}  steer: {}   mode: {}'.format(dataRX.drive, dataRX.steer, dataRX.mode)) # debug print
-                status.publish("Data converted")
+                print("Data converted")
 
                 if LoRaEnabled:
                     # Round data and test against deadzone
@@ -92,9 +91,9 @@ if __name__=='__main__':
                     #print("Drive: {}    Steer: {}".format(dataRX.drive,dataRX.steer)) # Test print statement
                     drive.publish(float(parameters[0]), float(parameters[1]), float(parameters[2]), float(parameters[3]), float(parameters[4]), float(parameters[5]))
                     steer.publish(float(parameters[6]), float(parameters[7]), float(parameters[8]), float(parameters[9]))
-                    status.publish("Publishing New Values")
+                    print("Publishing New Values")
             elif link.status < 0:   # Error codes for Serial Transfer Library 
-                status.publish("No Message Recieved")
+                print("No Message Recieved")
                 if link.status == txfer.CRC_ERROR:
                     print('ERROR: CRC_ERROR')
                 elif link.status == txfer.PAYLOAD_ERROR:
@@ -106,6 +105,6 @@ if __name__=='__main__':
                 drive.publish(0,0,0,0,0,0)
                 steer.publish(0,0,0,0)
     except rospy.ROSInterruptException:
-        status.publish("Ros Interrupt")
+        print("Ros Interrupt")
         link.close()    # Close serial connection
         pass
